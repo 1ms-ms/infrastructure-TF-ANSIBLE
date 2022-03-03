71decc77e4ac1e0d531f773579f4d627f3afaa6c
ms <vomircom@gmail.com>
2022-03-03 12:51:30 +0100

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "instance_count" {
+  default = "3"
+}
+
+variable "instance_tags" {
+  default = ["EC-1", "EC-2", "EC-3"]
+}
+variable "sub_tags"{
+  default = ["SUB-1","SUB-2","SUB-3"]
+}
+variable "VPC"{
+  type = string
+  default = "10.0.0.0/16"
+}
